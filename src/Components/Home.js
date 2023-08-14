218dba055ef66574dd015b26812324b8e5d4a8bd
Theodore Smith <tjs7321@gmail.com>
2023-08-14 13:21:07 -0400

    added various things

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Home(){
+function Home({rapture}){
     return(
     <>This is the Home Page</>
     )
--- src/Components/Plasmid.js
+++ src/Components/Plasmid.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Plasmid(){
+function Plasmid({rapture}){
     return(
     <>
     </>
