c447d741f8f62016358757887432a8c45a792d22
Theodore Smith <tjs7321@gmail.com>
2023-08-14 11:49:56 -0400

    added data.js and info

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const plasmids = [
+    {
+        id: 1,
+        title: {
+            rapture: "Electro Bolt",
+            columbia: "Shock Jockey"
+        },
+        tagline: {
+            rapture: "Stun your foes with a thousand volts!",
+            columbia: "The future of power!"
+        },
+        effect: {
+            rapture: "Sends a jolt of electricity from your fingertips",
+            columbia: "Sends a jolt of electricity from your fingertips"
+        }
+    },
+    {
+        id: 2,
+        title: {
+            rapture: "Incinerate",
+            columbia: "Devil's Kiss"
+        },
+        tagline: {
+            rapture: "Light up foes to a thousand degrees!",
+            columbia: "Light the way!"
+        },
+        effect: {
+            rapture: "Sets the target on fire",
+            columbia: "Throws a flaming projectile which detonates"
+        }
+    },
+    {
+        id: 3,
+        title: {
+            rapture: "Incinerate",
+            columbia: "Devil's Kiss"
+        },
+        tagline: {
+            rapture: "Light up foes to a thousand degrees!",
+            columbia: "Light the way!"
+        },
+        effect: {
+            rapture: "Sets the target on fire",
+            columbia: "Throws a flaming projectile which detonates"
+        }
+    },
+]
