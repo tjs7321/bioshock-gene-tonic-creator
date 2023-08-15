d7a004d32c9d7e50a19f8e093d31dc520e5e8f53
Theodore Smith <tjs7321@gmail.com>
2023-08-15 13:05:03 -0400

    fixed images in plasmid showcase, changed tab title and image

--- src/Components/Plasmid.js
+++ src/Components/Plasmid.js
@@ -4,7 +4,7 @@ function Plasmid({rapture, title, tagline, effect, image}){
     return(
     <>
         <h1>{rapture ? title.rapture : title.columbia}</h1>
-        <img src={rapture ? image.rapture : image.columbia}/>
+        <img src={rapture ? image.rapture : image.columbia} alt="plasmid/vigor icon"/>
         <p>{rapture ? tagline.rapture : tagline.columbia}</p>
         <p>{rapture ? effect.rapture : effect.columbia}</p>
     </>
