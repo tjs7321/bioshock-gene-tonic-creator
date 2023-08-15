38d673205758a3e3ab1ce8e8dac28c093511f22e
Theodore Smith <tjs7321@gmail.com>
2023-08-15 14:13:14 -0400

    did some other stuff

--- src/Components/Creator.js
+++ src/Components/Creator.js
@@ -1,8 +1,11 @@
 import React from "react";
+import NewPlasmidForm from "./NewPlasmidForm";
 
-function Creator(){
+function Creator({rapture}){
     return(
-    <>This is the Creator</>
+    <>{rapture ? "Welcome to Fontaine Futuristics" : "Welcome to Fink Manufacturing"}
+    <img src={rapture ? "https://static.wikia.nocookie.net/villains/images/6/6b/FontaineFuturisticsLogoBS2.png" : "https://static.wikia.nocookie.net/bioshock/images/a/a7/Fink_Manufacturing_logo.png"}/>
+    </>
     )
 }
 
