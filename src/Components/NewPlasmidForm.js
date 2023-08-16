e16c9d514163f5a25b721362a6f48e82855fa33d
Collin <shakescollin@aol.com>
2023-08-15 20:41:12 -0400

    added css to showroom comp

--- src/Components/NewPlasmidForm.js
+++ src/Components/NewPlasmidForm.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-const 
+
 
 function NewPlasmidForm(){
     return (
--- src/Components/Plasmid.js
+++ src/Components/Plasmid.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-function Plasmid({rapture, title, tagline, effect, image}){
-    return(
-    <>
-        <h1>{rapture ? title.rapture : title.columbia}</h1>
-        <img src={rapture ? image.rapture : image.columbia} alt="plasmid/vigor icon"/>
-        <p>{rapture ? tagline.rapture : tagline.columbia}</p>
-        <p>{rapture ? effect.rapture : effect.columbia}</p>
-    </>
-    )
+function Plasmid({ rapture, title, tagline, effect, image }) {
+  return (
+    <div className="plasmid">
+      <h1>{rapture ? title.rapture : title.columbia}</h1>
+      <img
+        src={rapture ? image.rapture : image.columbia}
+        alt="plasmid/vigor icon"
+      />
+      <p>{rapture ? tagline.rapture : tagline.columbia}</p>
+      <p className="effect">{rapture ? effect.rapture : effect.columbia}</p>
+    </div>
+  );
 }
 
-export default Plasmid
+export default Plasmid;
