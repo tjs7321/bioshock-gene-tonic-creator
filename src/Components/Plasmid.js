19537301a8c0f35baa3e6d33ef09e26afcbda4d0
Theodore Smith <tjs7321@gmail.com>
2023-08-15 23:02:10 -0400

    removed bootstrap

--- src/Components/Plasmid.js
+++ src/Components/Plasmid.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Plasmid({ rapture, title, tagline, effect, image }) {
   return (
-    <div className="plasmid">
+    <div class="col-sm-3">
       <h1>{rapture ? title.rapture : title.columbia}</h1>
       <img
         src={rapture ? image.rapture : image.columbia}
--- src/Components/Showroom.js
+++ src/Components/Showroom.js
@@ -3,39 +3,16 @@ import Plasmid from "./Plasmid";
 import { plasmids } from "../data";
 
 function Showroom({ rapture }) {
-  // Divide the plasmids into two arrays for the top and bottom rows
-  const topRowPlasmids = plasmids.slice(0, 5);
-  const bottomRowPlasmids = plasmids.slice(5, 8);
 
-  return (
-    <div className="container">
-      <h2 className="mt-4 mb-2">Plasmids</h2>
-      <table className="table table-bordered">
-        <tbody>
-          <tr>
-            {topRowPlasmids.map((plasmid) => (
-              <td key={plasmid.id} className="text-center">
-                <Plasmid rapture={rapture} {...plasmid} />
-              </td>
-            ))}
-          </tr>
-          <tr>
-            <td></td> {/* Empty cell */}
-            <td className="text-center">
-              <Plasmid rapture={rapture} {...bottomRowPlasmids[0]} />
-            </td>
-            <td className="text-center">
-              <Plasmid rapture={rapture} {...bottomRowPlasmids[1]} />
-            </td>
-            <td className="text-center">
-              <Plasmid rapture={rapture} {...bottomRowPlasmids[2]} />
-            </td>
-            <td></td> {/* Empty cell */}
-          </tr>
-        </tbody>
-      </table>
+  return(
+    <div class="row bg-primary">
+        {plasmids.map((plasmid) => <Plasmid
+        rapture={rapture}
+        {...plasmid}
+        key={plasmid.id}
+        />)}
     </div>
-  );
+    )
 }
 
 export default Showroom;
