1fd2dee85f6dd593576444d3ec822d289885636a
Theodore Smith <tjs7321@gmail.com>
2023-08-15 23:47:03 -0400

    minor changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,39 +1,28 @@
 import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
+import "../App.css";
 
 function NavBar({handleRaptureToggle}){
-
-    const linkStyles = {
-        display: "inline-block",
-        width: "30%",
-        padding: "12px",
-        margin: "0 6px 6px",
-        background: "blue",
-        textDecoration: "none",
-        color: "white",
-    };
-
-
 
     return (
         <nav class="navbar navbar-expand-sm navbar-light bg-primary">
             <NavLink
             to="/" exact
-            style={linkStyles}
+            className="navbarHeaders"
             activeStyle={{background: "darkblue"}}
             >
             Home
             </NavLink>
             <NavLink
             to="/showroom" exact
-            style={linkStyles}
+            className="navbarHeaders"
             activeStyle={{background: "darkblue"}}
             >
             Showroom
             </NavLink>
             <NavLink
             to="/creator" exact
-            style={linkStyles}
+            className="navbarHeaders"
             activeStyle={{background: "darkblue"}}
             >
             Creator
--- src/Components/Plasmid.js
+++ src/Components/Plasmid.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Plasmid({ rapture, title, tagline, effect, image }) {
   return (
-    <div class="col-sm-3">
+    <div className="plasmid">
       <h1>{rapture ? title.rapture : title.columbia}</h1>
       <img
         src={rapture ? image.rapture : image.columbia}
