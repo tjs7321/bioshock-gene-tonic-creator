2cccedbd3d1420508b07c64d997ecaf76f14edfc
Theodore Smith <tjs7321@gmail.com>
2023-08-16 22:48:52 -0400

    css changes

--- src/Components/Plasmid.js
+++ src/Components/Plasmid.js
@@ -4,11 +4,11 @@ function Plasmid({ rapture, title, tagline, effect, image }) {
   return (
     <div className={rapture?"plasmidCardRapture":"plasmidCardColumbia"}>
       <p className={rapture?"plasmidTitleRapture":"plasmidTitleColumbia"}>{rapture ? title.rapture : title.columbia}</p>
-      <img className="plasmidImage"
+      <img className={rapture?"plasmidImageRapture":"plasmidImageColumbia"}
         src={rapture ? image.rapture : image.columbia}
         alt="plasmid/vigor icon"
       />
-      <p className="tagline">{rapture ? tagline.rapture : tagline.columbia}</p>
+      <p className={rapture?"taglineRapture":"taglineColumbia"}>{rapture ? tagline.rapture : tagline.columbia}</p>
       {/* <p className="tagline">{rapture ? effect.rapture : effect.columbia}</p> */}
     </div>
   );
